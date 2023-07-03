65ed6cb0728c619e4ba65986500e8c046956d0cb
Pabitra Kumar Mohanty <77189083+pkmofficial@users.noreply.github.com>
2023-07-03 16:08:49 +0530

    Update index.js

--- pages/create-post/index.js
+++ pages/create-post/index.js
@@ -43,12 +43,17 @@ import Form from '../../components/CreatePost/Form';
 function CreatePost() {
   const { data: session } = useSession();
   const router = useRouter();
-
+  const showToastMessage = () => {
+    toast.warn('Please sign in to create a post.', {
+      position: "top-center", autoClose: 5000, hideProgressBar: false, closeOnClick: true, pauseOnHover: true, draggable: false, progress: undefined, theme: "light",
+    });
+  }
   useEffect(() => {
     if (!session) {
-      toast.warn('Please sign in to create a post.', {
-        position: "top-center", autoClose: 5000, hideProgressBar: false, closeOnClick: true, pauseOnHover: true, draggable: false, progress: undefined, theme: "light",
-      });
+      // toast.warn('Please sign in to create a post.', {
+      //   position: "top-center", autoClose: 5000, hideProgressBar: false, closeOnClick: true, pauseOnHover: true, draggable: false, progress: undefined, theme: "light",
+      // });
+      showToastMessage()
       // toast("");
       router.push('/'); // home page
     }
